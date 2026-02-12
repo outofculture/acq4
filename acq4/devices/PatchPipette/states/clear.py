24866e67b49d39377550150dd8808aee5a211955
Martin Chase <outofculture@gmail.com>
2026-02-12 09:41:23 -0800

    flesh out the Clear state

--- acq4/devices/PatchPipette/states/clear.py
+++ acq4/devices/PatchPipette/states/clear.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy
+
 from acq4.devices.PatchPipette.states import PatchPipetteState
 from acq4.util import ptime
 
@@ -7,6 +10,11 @@ class ClearState(PatchPipetteState):
 
     Parameters
     ----------
+    recoveryTimeout : float
+        Time (s) to spend trying to recover from a cell loss before giving up (default 30 s).
+    recoveryResistanceThresholdAbsolute : float
+        Resistance (Ohms) below which to consider the cell loss successfully reversed and
+        transition to 'whole cell' state (default 10 MΩ).
     """
 
     stateName = 'clear'
@@ -16,18 +24,63 @@ class ClearState(PatchPipetteState):
         'initialClampMode': 'VC',
         'initialVCHolding': -70e-3,
         'initialTestPulseEnable': True,
+        'fallbackState': 'fouled',
     }
-    _parameterTreeConfig = {}
+    _parameterTreeConfig = {
+        'recoveryTimeout': {'type': 'float', 'default': 60.0, 'suffix': 's'},
+        'recoveryResistanceThresholdAbsolute': {'type': 'float', 'default': 10e6, 'suffix': 'Ω', 'siPrefix': True},
+        'recoverySustainedTime': {'type': 'float', 'default': 2.0, 'suffix': 's'},
+    }
 
     def run(self):
+        # TODO relative R_acc threshold?
         start = ptime.time()
         tps = []
         while start + 2 > ptime.time():
             tps.extend(self.processAtLeastOneTestPulse())
-        baseline_rss = sum(tp['steady_state_resistance'] for tp in tps) / len(tps)
-        # Test both positive and negative pulses to see what is the effect on access resistance. E.g.:
-        #     -500 Pa pulse for 2 seconds, reading test pulses during this time
-        #     If effect was good + permanent, repeat
-        #     If effect was good but went away after releasing pressure, try -1 kPa
-        #     If effect was bad, try +500 Pa
-        #     etc..
+        best_r_acc = min(tp['access_resistance'] for tp in tps)
+        pressure = -500  # Pa
+        start = ptime.time()
+        sign_has_flipped = False
+        while start + self.config['recoveryTimeout'] > ptime.time():
+            first_recovery_time = None
+            while (
+                self.processAtLeastOneTestPulse()[-1].analysis['access_resistance']
+                < self.config['recoveryResistanceThresholdAbsolute']
+            ):
+                if first_recovery_time is None:
+                    first_recovery_time = ptime.time()
+                elif first_recovery_time + self.config['recoverySustainedTime'] < ptime.time():
+                    self.setState("cell recovered")
+                    # todo make sure when this lands in `main` we use the dict-style return
+                    return "whole cell"
+
+            pulse_start = ptime.time()
+            tps_during = []
+            self.waitFor(self.dev.pressureDevice.setPressure(pressure, source='regulator'))
+            while pulse_start + 2 > ptime.time():
+                tps_during.extend(self.processAtLeastOneTestPulse())
+            self.dev.pressureDevice.setPressure(0, source='atmosphere')
+            r_acc_during = np.asarray([tp['access_resistance'] for tp in tps_during])
+            time_during = np.asarray([tp['event_time'] for tp in tps_during])
+            slope = scipy.stats.linregress(r_acc_during, time_during).slope
+            if slope < 0:
+                tps_after = []
+                while pulse_start + 2 > ptime.time():
+                    tps_after.extend(self.processAtLeastOneTestPulse())
+                r_acc_after = np.asarray([tp['access_resistance'] for tp in tps_after])
+                if r_acc_after.mean() < best_r_acc:
+                    best_r_acc = r_acc_after.mean()
+                    # and repeat the same pressure
+                else:
+                    # increase the pressure magnitude
+                    pressure = np.sign(pressure) * (abs(pressure) + 500)
+            else:
+                if sign_has_flipped:
+                    # increase pressure magnitude and flip sign
+                    pressure = -1 * np.sign(pressure) * (abs(pressure) + 500)
+                else:
+                    # first flip keeps magnitude the same
+                    sign_has_flipped = True
+                    pressure = -1 * pressure
+        return self.config['fallbackState']
--- acq4/devices/PatchPipette/states/whole_cell.py
+++ acq4/devices/PatchPipette/states/whole_cell.py
@@ -9,8 +9,8 @@ class WholeCellState(PatchPipetteState):
 
     Parameters
     ----------
-    cellLossResistanceThreshold : float
-        If the pipette resistance rises above baseline plus this threshold (Ω), transition to
+    cellLossResistanceThresholdAbsolute : float
+        If the pipette resistance (Ra) rises above this threshold (Ω), transition to
         `cellLossState` (default 20 MΩ).
     cellLossState : str
         Name of state to transition to if possible cell loss is detected (default 'clear').
@@ -28,7 +28,7 @@ class WholeCellState(PatchPipetteState):
         'initialAutoBiasTarget': -70e-3,
     }
     _parameterTreeConfig = {
-        'cellLossResistanceThreshold': {'type': 'float', 'value': 20e6, 'suffix': 'Ω', 'siPrefix': True, 'step': 1e6},
+        'cellLossResistanceThresholdAbsolute': {'type': 'float', 'value': 20e6, 'suffix': 'Ω', 'siPrefix': True, 'step': 1e6},
         'cellLossState': {'type': 'str', 'value': 'clear'},
         'cellLossSustainedTime': {'type': 'float', 'value': 2.0, 'suffix': 's', 'step': 0.5},
     }
@@ -40,22 +40,22 @@ class WholeCellState(PatchPipetteState):
 
         # TODO: Option to switch to I=0 for a few seconds to get initial RMP decay
 
-        tps = []
-        start = ptime.time()
-        while start + 5 > ptime.time():
-            tps.extend(self.processAtLeastOneTestPulse())
+        # TODO relative R_acc threshold?
+        # tps = []
+        # start = ptime.time()
+        # while start + 5 > ptime.time():
+        #     tps.extend(self.processAtLeastOneTestPulse())
 
-        baseline_rss = sum(tp['steady_state_resistance'] for tp in tps) / len(tps)
-        threshold = baseline_rss + self.config['cellLossResistanceThreshold']
+        threshold = self.config['cellLossResistanceThresholdAbsolute']
         first_loss_time = None
         while True:
             tps = self.processAtLeastOneTestPulse()
-            rss = tps[-1]['steady_state_resistance']
-            if rss > threshold:
+            r_access = tps[-1].analysis['access_resistance']
+            if r_access > threshold:
                 if first_loss_time is None:
                     first_loss_time = ptime.time()
                 elif first_loss_time + self.config['cellLossSustainedTime'] < ptime.time():
-                    self.setState(f"cell loss in progress (resistance rose to {rss / 1e6:.1f} MΩ)")
+                    self.setState(f"cell loss in progress (R_acc rose to {r_access / 1e6:.1f} MΩ)")
                     # TODO make sure when this lands in `main` we use the dict-style return
                     return "clear"
             else:
